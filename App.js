7033abf359a57e5ea1ab92c4776582308819f59b
vermaprachi <109412846+vermaprachi@users.noreply.github.com>
2024-02-02 12:08:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import Todo from './Todo';
+
+const App = () =>{
+  return <>
+  
+  <div className="main_div">
+<div className="center_div">
+<br/>
+<h1>ToDo List</h1>
+<br/>
+<input type ="text" placeholder="Add a items"/>
+<button> + </button>
+
+</div>
+
+  </div>
+  </>;
+
+}
+export default App;
